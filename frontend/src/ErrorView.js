4a8a2a01f1a098e56f3069783cfe29fa77dc0aa6
Bo Henriksen <bomarconi@gmail.com>
2018-04-28 12:37:40 +0200

    ErrorView er lavet i den enkle udgave dvs hardcoded.

--- frontend/src/ErrorView.js
+++ frontend/src/ErrorView.js
@@ -3,10 +3,6 @@ import data from './dummyData/DummyErrorMessage.json';
 
 const ErrorView = () => {
     var errorMessages = data.errorMessages;
-    //var errorMessage = errorMessages.find(e => e.noConnection === "There is no connection to the backend.");
-         
-    /* console.log(errorMessages);
-    console.log(errorMessage); */
 
     const errorMessage = "There is no connection to the backend.";
 
